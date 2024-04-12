737e0bda7748843b221bf680225124a15b3233e8
Julio Holon <juliolrmonteiro@gmail.com>
2024-04-12 17:39:44 -0300

    adding dotenv

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ setInterval(() => {
 */
 
 /* ============================================================================================================================================================== */
-
+require('dotenv').config()
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const fs = require("fs");
